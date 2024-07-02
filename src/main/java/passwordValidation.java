a385d3d9cf9234c7c69911f035416aa900bd26fa
Marcel Herr <mrclhrr@gmail.com>
2024-07-02 10:37:48 +0200

    add password length test and methode

--- src/main/java/passwordValidation.java
+++ src/main/java/passwordValidation.java
@@ -2,4 +2,12 @@ public class passwordValidation {
     public static void main(String[] args) {
         System.out.println("Enter password");
     }
+
+    public static boolean checkLength(String password) {
+        if (password.length() >= 8) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/test/java/passwordValidationTest.java
+++ src/test/java/passwordValidationTest.java
@@ -1,5 +1,36 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class passwordValidationTest {
 
+    @Test
+    public void passwordLength_when8_thenReturnTrue() {
+        //Given
+        String testPassword = "12345678";
+        //When
+        boolean acutal = passwordValidation.checkLength(testPassword);
+        //Then
+        assertTrue(acutal);
+    }
+
+    @Test
+    public void passwordLength_when5_thenReturnFalse() {
+        //Given
+        String testPassword = "12345";
+        //When
+        boolean acutal = passwordValidation.checkLength(testPassword);
+        //Then
+        assertFalse(acutal);
+    }
+
+    @Test
+    public void passwordLength_when10_thenReturnTrue() {
+        //Given
+        String testPassword = "1234567890";
+        //When
+        boolean acutal = passwordValidation.checkLength(testPassword);
+        //Then
+        assertTrue(acutal);
+    }
 }
